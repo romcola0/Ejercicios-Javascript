54d90328974a0a7da2217e3720edd4d79c2ef000
Rommel <wromw22@gmail.com>
2026-04-27 22:01:42 +0200

    Largest Number Finder

---
+++ FreeCodeCamp/FCCLabFundamentals2.js
@@ -0,0 +1,15 @@
+function largestOfAll(arr) {
+  const results = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let largestNumber = arr[i][0];
+
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+    results.push(largestNumber);
+  }
+  return results;
+}
