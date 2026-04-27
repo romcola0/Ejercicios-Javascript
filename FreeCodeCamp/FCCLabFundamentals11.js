e638b80d7e41bf54a8136483c42a1524ced056c6
Rommel <wromw22@gmail.com>
2026-04-27 22:04:32 +0200

    Password Generator

---
+++ FreeCodeCamp/FCCLabFundamentals11.js
@@ -0,0 +1,17 @@
+function generatePassword(length) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+
+    const randomIndex = Math.floor(Math.random() * characters.length);
+
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+const password = generatePassword(12);
+
+console.log("Generated password: " + password); 
